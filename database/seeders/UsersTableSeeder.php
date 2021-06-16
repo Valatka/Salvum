2073f4df700719dd8c3acabf3c0adff7fca08f9d
Benas <valatkabenas@gmail.com>
2021-06-16 16:32:49 +0300

    add: jwt auth

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'email' => 'test@test.com',
+            'password' => bcrypt('test'),
+        ]);
+    }
+}
